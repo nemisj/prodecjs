b68768aa5ded0de34e484fc4e8100fe2f944acbc
Maks Nemisj <tableton@mail.ru>
2010-07-07 00:15:53 +0200

    Some changes with documentation

--- test-test-1.js
+++ test-test-1.js
@@ -1,16 +1,22 @@
 var child = new Sub();
-// First public methods
-// result should be "setted"
 
-var old = child.getValue();
+// public overriden function 
+// with supercall
+debug('\nOverride + supercall:');
+ child.getValue();
 
+// inherited function
+// with protected overrriden function
+debug('\nInherit + protect:');
 child.setValue('test');
 
+debug('\nValue test');
 var newv = child.getValue();
 
 // Testing protected method from outside
 // Method should throw error
 try {
+    debug('\nProtected access');
     child._setValue( 'never' );
 } catch(e) {
     debug("ERROR: " + e.message);
